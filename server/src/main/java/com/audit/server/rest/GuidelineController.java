57bf912152e931452adb4c4e198e820b17dd3a93
Kaya <seyma.kaya@capgemini.com>
2026-03-13 16:20:15 +0100

    feat: API for guideline id added

--- server/src/main/java/com/audit/server/rest/GuidelineController.java
+++ server/src/main/java/com/audit/server/rest/GuidelineController.java
@@ -4,10 +4,13 @@ import com.audit.server.model.Guideline;
 import com.audit.server.repo.GuidelineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/guidelines")
@@ -19,6 +22,23 @@ public class GuidelineController {
     @GetMapping(path = "/all", produces = "application/json")
     public List<Guideline> getAllGuidelines() {
         return repo.findAll();
+    }
+
+    @GetMapping(path = "/ids", produces = "application/json")
+    public List<String> getAllIds() {
+        List<Guideline> guidelines = repo.findAll();
+        List<String> ids = new ArrayList<>();
+
+        for (Guideline g : guidelines){
+            ids.add(String.valueOf(g.getId()));
+        }
+
+        return ids;
+    }
+
+    @GetMapping(path = "/{id}", produces = "application/json")
+    public Optional<Guideline> getById(@PathVariable String id){
+        return repo.findById(id);
     }
 }
 
