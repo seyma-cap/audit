80b3de87c1dd35c953af3bc3b3ac7708297358a6
Kaya <seyma.kaya@capgemini.com>
2026-04-22 11:36:43 +0200

    feat: nav goes to homepage

--- app/src/component/navbar.js
+++ app/src/component/navbar.js
@@ -1,10 +1,17 @@
 import '../style/navbar.css';
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
+    const navigate = useNavigate();
+
+    function goHome(){
+        navigate("/");
+    }
+
   return (
     <div className="container">
         <div className="bar">
-            <p className="title">Accessibility audit</p>
+            <p onClick={goHome} className="title">Accessibility audit</p>
         </div>
      </div>
   );
